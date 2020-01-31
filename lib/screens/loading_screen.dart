ead0e9b267bd309d48e166002d66ea27b5728c09
alceil <ashishshajisrampickal@gmail.com>
2020-01-31 08:17:29 +0530

    modified

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+const apiKey='b0b429eedb42a54045f4ae96b69898da';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -9,6 +11,9 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
+
+  double latitude;
+  double longitude;
   @override
   void initState() {
     // TODO: implement initState
@@ -21,14 +26,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getlocation() async {
     Location location = location();
     await location.getcurrentlocation();
-    print(location.latitude);
-    print(location.longitude)
+    latitude = location.latitude;
+    longitude = location.longitude;
  }
  void getData() async{
-      http.Response response = await http.get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22'),
+      http.Response response = await http.get('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=b6907d289e10d714a6e88b30761fae22'),
     if(response.statuscode==200)
    {
    String data=response.body;
+   var temp = jsonDecode(data)['main']['temp'];
+   }
    else
      {
        print(response.statuscode)
